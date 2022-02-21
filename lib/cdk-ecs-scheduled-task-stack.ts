dfdd805629608890b8196fc72eee7453cfc5825f
Duleendra Shashimal <duleendra@bambu.co>
2022-02-22 00:27:39 +0800

    Implemented ECS scheduled task

--- lib/cdk-ecs-scheduled-task-stack.ts
+++ lib/cdk-ecs-scheduled-task-stack.ts
@@ -1,16 +1,43 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Stack, StackProps} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import {InstanceType, NatProvider, SubnetType, Vpc} from "aws-cdk-lib/aws-ec2";
+import {Cluster, ContainerImage, FargatePlatformVersion, LogDriver} from "aws-cdk-lib/aws-ecs";
+import {ScheduledFargateTask} from "aws-cdk-lib/aws-ecs-patterns";
+import {Repository} from "aws-cdk-lib/aws-ecr";
+import {Schedule} from "aws-cdk-lib/aws-events";
 
 export class CdkEcsScheduledTaskStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
 
-    // The code that defines your stack goes here
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkEcsScheduledTaskQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        //Creating the VPC
+        const vpc = new Vpc(this, 'VPCForECS', {
+            maxAzs: 2,
+            natGatewayProvider: NatProvider.instance({
+                instanceType: new InstanceType('t2.micro'),
+            })
+        });
+
+        //Creating the ECS cluster
+        const cluster = new Cluster(this, 'ECSCluster', {
+            vpc
+        });
+
+        //Creating a Fargate schedueld task
+        const scheduledTask = new ScheduledFargateTask(this, 'ScheduledTask', {
+            cluster: cluster,
+            platformVersion: FargatePlatformVersion.LATEST,
+            desiredTaskCount: 2,
+            schedule: Schedule.expression('cron(0/10 * * * ? *)'),
+            subnetSelection: {subnetType: SubnetType.PRIVATE_WITH_NAT},
+            scheduledFargateTaskImageOptions: {
+                image: ContainerImage.fromEcrRepository(Repository.fromRepositoryName(this, `ECR-Image`, 'test-cron-job')),
+                memoryLimitMiB: 512,
+                cpu: 256,
+                command: ['node', 'app.js'],
+                logDriver: LogDriver.awsLogs({streamPrefix: 'ScheduledTaskLogs'})
+            }
+        });
+    }
 }
